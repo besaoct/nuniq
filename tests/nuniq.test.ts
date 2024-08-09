1eccb384859e1109d7a25f47c702e5af86e3eb5c
Shafin <blackipie@outlook.com>
2024-08-09 06:40:19 +0530

    nuniq v1.0.0

--- tests/nuniq.test.ts
+++
@@ -1,9 +0,0 @@
-// src/nuniq.test.ts
-import { nuniq, Config } from './../src';
-test('should generate a random string if no valid function is provided', () => {
-    const config = { fn: 'nonExistentFunction' } as Config;
-  
-    const result = nuniq(config);
-    expect(result).toMatch(/^[a-z0-9]{12}$/);
-  });
-  
