621bf052a0c3c03d716e054bd1ab0393919a7d1b
Shafin <blackipie@outlook.com>
2024-08-10 13:32:31 +0530

    🐛 fixed

--- build.js
+++ build.js
@@ -6,9 +6,23 @@ esbuild.build({
   bundle: true,
   platform: 'node',
   target: ['node14'],
+  format: 'cjs',
+  plugins: [dtsPlugin()],
+  outdir: './dist/cjs',
+  sourcemap: false,
+  minify: true,
+  tsconfig:'./tsconfig.json'
+}).catch(() => process.exit(1));
+
+
+esbuild.build({
+  entryPoints: ['./src/index.ts'],
+  bundle: true,
+  platform: 'node',
+  target: ['node14'],
   format: 'esm',
   plugins: [dtsPlugin()],
-  outdir: './dist',
+  outdir: './dist/esm',
   sourcemap: false,
   minify: true,
   tsconfig:'./tsconfig.json'
